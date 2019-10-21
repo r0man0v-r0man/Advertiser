7300238d18124f3cfea61a4c1fb40d89c02629db
Roman <romanovmailbox@mail.ru>
2019-10-21 22:12:33 +0300

    init ng zorro

--- Advertiser/ClientApp/src/app/app.module.ts
+++ Advertiser/ClientApp/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, ru_RU } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
@@ -10,9 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: ru_RU }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
